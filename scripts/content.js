e1eb7a6134c5b721d24da1f1630dff89718755a8
Rushil Punchhi <rushilxo5@gmail.com>
2022-12-05 01:33:47 -0800

    mess

--- scripts/content.js
+++ scripts/content.js
@@ -1,9 +1,63 @@
-console.log("content.js loaded");
-$(document).keydown(function (e) {
-    console.log("keydown");
-    console.log("keycode: "+e.keyCode);
-    console.log("timestamp:" +e.timeStamp);
-    console.log("key: "+e.key);
-});
+// console.log("content.js loaded");
+// (() => {
+//   chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
+//     let domain = request.domain;
+//     console.log("Content domain: " + domain);
+//   });
 
+//   function sleepRand(time) {
+//     var now = new Date().getTime();
+//     var end = now + Math.random() * parseInt(time) + 1;
+//     while (new Date().getTime() < end) {
+//       /* do nothing */
+//     }
+//   }
 
+//   sendResponse({ message: "ok" });
+//   $(":input:text").keydown(function (e) {
+//     var shouldSleep =
+//       Math.round(Math.random()) < 0.5 ? sleepRand(request.KP_dwelltime) : false;
+//   });
+
+//   $(":input:text").keyup(function (e) {
+//     var shouldSleep =
+//       Math.round(Math.random()) < 0.5 ? sleepRand(request.KP_gaptime) : false;
+//   });
+
+//   $("input[type=text], textarea").keyup(function (e) {
+//     var shouldSleep =
+//       Math.round(Math.random()) < 0.5 ? sleepRand(request.KP_gaptime) : false;
+//   });
+
+//   chrome.runtime.onMessage.addListener(function (
+//     request,
+//     sender,
+//     sendResponse
+//   ) {
+//     if (request.name == "enablePrivacyMode") {
+//       sendResponse({ message: "ok" });
+//       $(":input:text").keydown(function (e) {
+//         var now = new Date().getTime();
+//         var end = now + 1000;
+//         console.log("now: " + now);
+//         console.log("End: " + end);
+//         while (new Date().getTime() < end) {
+//           /* do nothing */
+//         }
+
+//         console.log("keydown");
+//         console.log("keycode: " + e.keyCode);
+//         console.log("timestamp:" + e.timeStamp);
+//         console.log("key: " + e.key);
+//       });
+
+//       $(":input:text").keyup(function (e) {
+//         console.log("keyup");
+//       });
+
+//       $("input[type=text], textarea").keyup(function (e) {
+//         console.log("keyup2");
+//       });
+//     }
+//   });
+// })();
--- scripts/popup.js
+++ scripts/popup.js
@@ -1,47 +1,67 @@
-let enabled = false;
-
-var togglePrivacy = function () {
-  if (!enabled) {
-    alert("Privacy is now enabled");
-    enabled = true;
-    // setEnabledPrivacy();
-    // removeWhitelistSite(siteURL);
+var togglePrivacy = async function () {
+  let status = await chrome.storage.sync.get(["privacyStatus"]);
+  if (status.privacyStatus === "enabled") {
+    chrome.storage.sync.set({ privacyStatus: "disabled" });
   } else {
-    alert("Privacy is now disabled");
-    enabled = false;
-    // setDisabledPrivacy();
-    // whitelistSite(siteURL);
+    chrome.storage.sync.set({ privacyStatus: "enabled" });
+    chrome.runtime.sendMessage({ name: "enablePrivacyMode" });
   }
+  chrome.tabs.getSelected(null, function(tab) {
+    chrome.tabs.reload(tab.id);
+    setTimeout(function() {window.close();}, 1000);
+});
 };
 
-$("#togglePrivacy").on("click", function (e) {
-  console.log("togglePrivacy clicked");
-  e.preventDefault();
-  togglePrivacy();
-  // chrome.tabs.getSelected(null, function(tab) {
-  //     chrome.tabs.reload(tab.id);
-  //     setTimeout(function() {window.close();}, 1000);
-  // });
+
+document.addEventListener("DOMContentLoaded", function () {
+  chrome.storage.sync.get({"privacyStatus": 'disabled'}).then((result) => {
+    console.log("Value currently is " + result.privacyStatus);
+    let toggleButton = document.getElementById("toggleButton");
+    if (result.privacyStatus == "enabled")
+      toggleButton.checked = true;
+    else if (result.privacyStatus == "disabled")
+      toggleButton.checked = false;
+  
+  });
+  document
+    .getElementById("togglePrivacy")
+    .addEventListener("click", togglePrivacy);
 });
 
-$(":input:text").keydown(function (e) {
-  var now = new Date().getTime();
-	var end = (now + 1000);
-  console.log("now: "+now);
-  console.log("End: " + end);
-  while(new Date().getTime() < end){ /* do nothing */ }
 
 
-  console.log("keydown");
-  console.log("keycode: " + e.keyCode);
-  console.log("timestamp:" + e.timeStamp);
-  console.log("key: " + e.key);
-});
+// $("#togglePrivacy").on("click", function (e) {
+//   console.log("togglePrivacy clicked");
+//   e.preventDefault();
+//   togglePrivacy();
+//   // chrome.tabs.getSelected(null, function(tab) {
+//   //     chrome.tabs.reload(tab.id);
+//   //     setTimeout(function() {window.close();}, 1000);
+//   // });
+// });
 
-$(":input:text").keyup(function(e) {
-  console.log("keyup");
-});
+// $("#togglePrivacy").on("click", function (e) {
+//   console.log("togglePrivacy clicked");
+//   togglePrivacy();
+// });
 
-$('input[type=text], textarea').keyup(function(e) {
-  console.log("keyup2");
-});
+// $(":input:text").keydown(function (e) {
+//   var now = new Date().getTime();
+// 	var end = (now + 1000);
+//   console.log("now: "+now);
+//   console.log("End: " + end);
+//   while(new Date().getTime() < end){ /* do nothing */ }
+
+//   console.log("keydown");
+//   console.log("keycode: " + e.keyCode);
+//   console.log("timestamp:" + e.timeStamp);
+//   console.log("key: " + e.key);
+// });
+
+// $(":input:text").keyup(function(e) {
+//   console.log("keyup");
+// });
+
+// $('input[type=text], textarea').keyup(function(e) {
+//   console.log("keyup2");
+// })
