5320fcef0de4ac6374bbee84318fcaf975bf70ce
Rushil Punchhi <rushilxo5@gmail.com>
2022-12-08 13:36:23 -0800

    profiles, new UI

--- src/background.js
+++ src/background.js
@@ -5,74 +5,56 @@
 // For more information on background script,
 // See https://developer.chrome.com/extensions/background_pages
 
-chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
-  if (request.type === 'GREETINGS') {
-    const message = `Hi ${
-      sender.tab ? 'Con' : 'Pop'
-    }, my name is Bac. I am from Background. It's great to hear from you.`;
+import * as tf from '@tensorflow/tfjs';
+console.log('This is background service worker - edit me!');
 
-    // Log message coming from the `request` parameter
-    console.log(request.payload.message);
-    // Send a response message
-    sendResponse({
-      message,
+function computePrediction(model, host) {
+  chrome.storage.sync.get(host).then((result) => {
+    if (result !== undefined) {
+      console.log('Website already exists. Returning prediction');
+      console.log(result[host]);
+      return result[host];
+    }
+
+    console.log('New website. Generating new prediction');
+    let prediciion = model
+      .predict(tf.randomUniform([1, 1, 27], 0, 1))
+      .dataSync();
+    const payload = Array.from(prediciion);
+    chrome.storage.sync.set({ [host]: payload }).then(() => {
+      console.log('Prediction is set to ' + payload);
+      return payload;
     });
-  }
-});
-
-import * as tf from "@tensorflow/tfjs";
-console.log("This is background service worker - edit me!");
-
-// chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
-//   if (request.type === 'GREETINGS') {
-//     const message = `Hi ${
-//       sender.tab ? 'Con' : 'Pop'
-//     }, my name is Bac. I am from Background. It's great to hear from you.`;
-
-//     // Log message coming from the `request` parameter
-//     console.log(request.payload.message);
-//     // Send a response message
-//     sendResponse({
-//       message,
-//     });
-//   }
-// });
-
-
-
-
-  
+  });
+}
 
 async function tensorflow() {
-
   // Load the model.
-  const model = await tf.loadGraphModel("http://localhost:8080/model.json");
-  let prediciion = model.predict(tf.zeros([1, 1, 27])).dataSync();
-  const payload = Array.from(prediciion)
+  const model = await tf.loadGraphModel('http://localhost:8080/model.json');
+  console.log('Model loaded');
 
   chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
     if (request.type === 'REQUEST_PREDICTION') {
-      const message = "Hi bit"
-
+      const message = 'Hi bit, computing prediction';
       // Log message coming from the `request` parameter
       console.log(request.payload.message);
+      console.log('host in tf:' + request.payload.host);
       // Send a response message
       sendResponse({
         message,
-        payload
       });
     }
-  })
-
-
-
+  });
 }
-async function loadmodel(){
-  const model = await tf.loadGraphModel("http://localhost:8080/model.json");
-  let prediciion = model.predict(tf.zeros([1, 1, 27])).dataSync();
-  const predArray = Array.from(prediciion)
-  console.log(predArray)
-
-}
-
 tensorflow();
+
+chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
+  if (changeInfo.status !== 'complete') return;
+  //get hostname
+  let hostname = new URL(tab.url).hostname;
+  // Enable Privacy
+  chrome.tabs.sendMessage(tabId, {
+    type: 'enablePrivacy',
+    payload: { host: hostname },
+  });
+});
--- src/contentScript.js
+++ src/contentScript.js
@@ -17,19 +17,6 @@ console.log(
   `Page title is: '${pageTitle}' - evaluated by Chrome extension's 'contentScript.js' file`
 );
 
-// Communicate with background file by sending a message
-chrome.runtime.sendMessage(
-  {
-    type: 'GREETINGS',
-    payload: {
-      message: 'Hello, my name is Con. I am from ContentScript.',
-    },
-  },
-  (response) => {
-    console.log(response.message);
-  }
-);
-
 function sleepRand(time) {
   var now = new Date().getTime();
   var end = now + Math.random() * parseInt(time) + 1;
@@ -46,64 +33,41 @@ function sleep(ms) {
   }
 }
 
-chrome.runtime.sendMessage(
-  {
-    type: 'REQUEST_PREDICTION',
-    payload: {
-      message: 'Hello, my name is Con. Pls give prediction.',
+chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
+  if (request.type === 'enablePrivacy') runPrivacy(request.payload.host);
+});
+
+function runPrivacy(host) {
+  chrome.runtime.sendMessage(
+    {
+      type: 'REQUEST_PREDICTION',
+      payload: {
+        message: 'This is Con. Pls give prediction.',
+        host: host,
+      },
     },
-  },
-  (response) => {
-    console.log(response.message);
-    console.log(response.payload);
+    (response) => {
+      console.log(response.message);
+      console.log(response.payload);
 
-    function detsleep(keycode) {
-      if (keycode == 32) sleep(response.payload[26]);
-      else if (keycode >= 65 && keycode <= 90)
-        sleep(response.payload[keycode - 65]);
-      else sleep(response.payload[26]);
+      function detsleep(keycode) {
+        if (keycode == 32) sleep(response.payload[26]);
+        else if (keycode >= 65 && keycode <= 90)
+          sleep(response.payload[keycode - 65]);
+        else sleep(response.payload[26]);
+      }
+      $(':input:text').keydown(function (e) {
+        console.log(e.keyCode);
+        detsleep(e.keyCode);
+      });
+      $(':input:text').keyup(function (e) {
+        console.log('2');
+        detsleep(e.keyCode);
+      });
+      $('input[type=text], textarea').keyup(function (e) {
+        console.log('3');
+        detsleep(e.keyCode);
+      });
     }
-    $(':input:text').keydown(function (e) {
-      console.log(e.keyCode);
-      detsleep(e.keyCode);
-    });
-    $(':input:text').keyup(function (e) {
-      console.log("2");
-      detsleep(e.keyCode);
-    });
-    $('input[type=text], textarea').keyup(function (e) {
-      console.log("3");
-      detsleep(e.keyCode);
-    });
-  }
-
-    
- 
-);
-
-// $(':input:text').keydown(function (e) {
-//   var shouldSleep =
-//     Math.round(Math.random()) < 0.5 ? sleepRand(200) : false;
-// });
-
-// $(':input:text').keyup(function (e) {
-//   var shouldSleep =
-//     Math.round(Math.random()) < 0.5 ? sleepRand(200) : false;
-// });
-
-// $('input[type=text], textarea').keyup(function (e) {
-//   var shouldSleep =
-//     Math.round(Math.random()) < 0.5 ? sleepRand(200) : false;
-// });
-
-// // Listen for message
-// chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
-//   if (request.type === 'COUNT') {
-//     console.log(`Current count is ${request.payload.count}`);
-//   }
-
-//   // Send an empty response
-//   // See https://github.com/mozilla/webextension-polyfill/issues/130#issuecomment-531531890
-//   sendResponse({});
-//   return true;
-// });
+  );
+}
